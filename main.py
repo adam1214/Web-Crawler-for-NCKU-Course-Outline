26c1384ac8ce2452be0b3e377e102574e44a0a0c
adam1214 <34540871+adam1214@users.noreply.github.com>
2020-04-04 22:33:08 +0800

    change "Grading" and "Teaching Strategies" string format

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+'''
+# -*-coding:utf-8 -*
+import sys  
+reload(sys)
+sys.setdefaultencoding('utf-8')
+'''
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import csv
+import re
+from string import digits
 
 '''
 w：新建檔案寫入(檔案可不存在，若存在則清空，適用於第一次寫入的資料)
 a：資料附加到舊檔案後面(游標指在EOF)
 '''
+
+# 判斷是否為數字
+def is_number(num):
+  pattern = re.compile(r'^[-+]?[-0-9]\d*\.\d*|[-+]?\.?[0-9]\d*$')
+  result = pattern.match(num)
+  if result:
+    return True
+  else:
+    return False
 
 # 下載Course Outline內容
 r = requests.get('http://class-qry.acad.ncku.edu.tw/syllabus/online_display.php?syear=0108&sem=2&co_no=F731700&class_code=')
@@ -21,6 +38,7 @@ References = ""
 Description = ""
 Objectives = ""
 Outline = ""
+
 
 # 確認是否下載成功
 if r.status_code == requests.codes.ok:
@@ -47,11 +65,34 @@ if r.status_code == requests.codes.ok:
 
         if div.text.find('評量方式') != -1:
             #print(div.text)
-            Grading = div.text
+            str = div.text.splitlines()
+            index = len(str) - 3
+            L1 = [None] * (index-6)
+            L2 = [None] * (index-6)
+            for i in range(6,index):
+                num = ''.join([x for x in str[i] if x.isdigit()])
+                L1[i-6] = str[i].replace(num,'')
+            for i in range(7,index+1):
+                num = ''.join([x for x in str[i] if x.isdigit()])
+                L2[i-7] = num
+
+            Grading = list(zip(L1,L2))
 
         if div.text.find('教學方法') != -1:
-            #print(div.text)           
-            Strategies = div.text
+            #print(div.text)
+            str = div.text.splitlines()  
+
+            index = len(str) - 3
+            L1 = [None] * (index-5)
+            L2 = [None] * (index-5)
+            for i in range(5,index):
+                num = ''.join([x for x in str[i] if x.isdigit()])
+                L1[i-5] = str[i].replace(num,'')
+            for i in range(6,index+1):
+                num = ''.join([x for x in str[i] if x.isdigit()])
+                L2[i-6] = num
+
+            Strategies = list(zip(L1,L2))
 
         if div.text.find('課程教材') != -1:
             #print(div.text)
@@ -81,6 +122,7 @@ if r.status_code == requests.codes.ok:
 
             a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
             b = str.split('#')
+
             Outline = list(zip(a,b))
 
     with open('result.csv', 'w', newline='') as csvfile:
