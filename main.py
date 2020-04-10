11bfc1a86f7623a4343fcb76516d575b0d9e34b8
adam1214 <34540871+adam1214@users.noreply.github.com>
2020-04-10 22:09:30 +0800

    With some error about Outline

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import re
 from string import digits
 
 '''
+Windows環境中，若程式出現"UnicodeEncodeError: ‘cp950’ codec can’t encode character"的錯誤，可於terminal輸入"chcp 65001"來解決
 w：新建檔案寫入(檔案可不存在，若存在則清空，適用於第一次寫入的資料)
 a：資料附加到舊檔案後面(游標指在EOF)
 '''
@@ -25,109 +26,124 @@ def is_number(num):
   else:
     return False
 
-# 下載Course Outline內容
-r = requests.get('http://class-qry.acad.ncku.edu.tw/syllabus/online_display.php?syear=0108&sem=2&co_no=F731700&class_code=')
-r.encoding='big5'
+with open('ncku_course.csv', 'r', newline='', encoding="utf-8") as csvfile:
+    # 讀取 CSV 檔案內容
+    rows = csv.reader(csvfile)
+    row_num = 1
+    # 以迴圈讀取每一列
+    for row in rows:
+        print(row_num)
+        if row_num == 4:
+            break
+        if row_num == 1:
+            with open('result.csv', 'w', newline='') as tablefile:
+                # 建立 CSV 檔寫入器
+                writer = csv.writer(tablefile)
+                writer.writerow(["學院","系所名稱","系號-序號","課程碼-分班碼","屬性碼","年級","類別","科目名稱","學分","必/選修","教師姓名","已選課人數/餘額","時間","教室","課程大綱","先修課程或先備能力","教師聯絡資訊","評量方式","教學方法","課程教材","參考書目","課程概述","課程學習目標","課程進度"])
+            tablefile.close()
+        else:
+            Prerequisite = ""
+            Contact = ""
+            Grading = ""
+            Strategies = ""
+            Material = ""
+            References = ""
+            Description = ""
+            Objectives = ""
+            Outline = ""
 
-Prerequisite = ""
-Contact = ""
-Grading = ""
-Strategies = ""
-Material = ""
-References = ""
-Description = ""
-Objectives = ""
-Outline = ""
+            # 下載Course Outline內容
+            r = requests.get(row[14])
+            r.encoding='big5'
 
+            # 確認是否下載成功
+            if r.status_code == requests.codes.ok:
+                # 以 BeautifulSoup 解析 HTML 程式碼
+                soup = BeautifulSoup(r.text, 'html.parser')
+                divs = soup.find_all('div', 'block')
 
-# 確認是否下載成功
-if r.status_code == requests.codes.ok:
-    # 以 BeautifulSoup 解析 HTML 程式碼
-    soup = BeautifulSoup(r.text, 'html.parser')
-    divs = soup.find_all('div', 'block')
+                for div in divs:
 
-    for div in divs:
+                    if div.text.find('先修課程或先備能力') != -1:
+                        Prerequisite = div.text[1:]
 
-        if div.text.find('先修課程或先備能力') != -1:
-            Prerequisite = div.text[1:]
+                    if div.text.find('教師聯絡資訊') != -1:
+                        Contact = div.text[1:]
 
-        if div.text.find('教師聯絡資訊') != -1:
-            Contact = div.text[1:]
+                    if div.text.find('參考書目') != -1:
+                        References = div.text[1:]
 
-        if div.text.find('參考書目') != -1:
-            References = div.text[1:]
+                    if div.text.find('課程概述') != -1:
+                        Description = div.text[1:]
 
-        if div.text.find('課程概述') != -1:
-            Description = div.text[1:]
+                    if div.text.find('課程學習目標') != -1:
+                        Objectives = div.text[1:]
 
-        if div.text.find('課程學習目標') != -1:
-            Objectives = div.text[1:]
+                    if div.text.find('評量方式') != -1:
+                        #print(div.text)
+                        str = div.text.splitlines()
+                        index = len(str) - 3
+                        L1 = [None] * (index-6)
+                        L2 = [None] * (index-6)
+                        for i in range(6,index):
+                            num = ''.join([x for x in str[i] if x.isdigit()])
+                            L1[i-6] = str[i].replace(num,'')
+                        for i in range(7,index+1):
+                            num = ''.join([x for x in str[i] if x.isdigit()])
+                            L2[i-7] = num
+                        Grading = list(zip(L1,L2))
 
-        if div.text.find('評量方式') != -1:
-            #print(div.text)
-            str = div.text.splitlines()
-            index = len(str) - 3
-            L1 = [None] * (index-6)
-            L2 = [None] * (index-6)
-            for i in range(6,index):
-                num = ''.join([x for x in str[i] if x.isdigit()])
-                L1[i-6] = str[i].replace(num,'')
-            for i in range(7,index+1):
-                num = ''.join([x for x in str[i] if x.isdigit()])
-                L2[i-7] = num
+                    if div.text.find('教學方法') != -1:
+                        #print(div.text)
+                        str = div.text.splitlines()  
 
-            Grading = list(zip(L1,L2))
+                        index = len(str) - 3
+                        L1 = [None] * (index-5)
+                        L2 = [None] * (index-5)
+                        for i in range(5,index):
+                            num = ''.join([x for x in str[i] if x.isdigit()])
+                            L1[i-5] = str[i].replace(num,'')
+                        for i in range(6,index+1):
+                            num = ''.join([x for x in str[i] if x.isdigit()])
+                            L2[i-6] = num
 
-        if div.text.find('教學方法') != -1:
-            #print(div.text)
-            str = div.text.splitlines()  
+                        Strategies = list(zip(L1,L2))
 
-            index = len(str) - 3
-            L1 = [None] * (index-5)
-            L2 = [None] * (index-5)
-            for i in range(5,index):
-                num = ''.join([x for x in str[i] if x.isdigit()])
-                L1[i-5] = str[i].replace(num,'')
-            for i in range(6,index+1):
-                num = ''.join([x for x in str[i] if x.isdigit()])
-                L2[i-6] = num
+                    if div.text.find('課程教材') != -1:
+                        #print(div.text)
+                        Material = div.text[1:]
+                        
+                    if div.text.find('課程進度') != -1:
+                        str = div.text.splitlines()[5]
+                        str=str.replace("10", "#")
+                        str=str.replace("11", "#")
+                        str=str.replace("12", "#")
+                        str=str.replace("13", "#")
+                        str=str.replace("14", "#")
+                        str=str.replace("15", "#")
+                        str=str.replace("16", "#")
+                        str=str.replace("17", "#")
+                        str=str.replace("18", "#")
+                        str=str.replace("1", "")
+                        str=str.replace("2", "#")
+                        str=str.replace("3", "#")
+                        str=str.replace("4", "#")
+                        str=str.replace("5", "#")
+                        str=str.replace("6", "#")
+                        str=str.replace("7", "#")
+                        str=str.replace("8", "#")
+                        str=str.replace("9", "#")
+                        str=str.replace("##", "#")
 
-            Strategies = list(zip(L1,L2))
+                        a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+                        b = str.split('#')
 
-        if div.text.find('課程教材') != -1:
-            #print(div.text)
-            Material = div.text[1:]
-           
-        if div.text.find('課程進度') != -1:
-            str = div.text.splitlines()[5]
-            str=str.replace("10", "#")
-            str=str.replace("11", "#")
-            str=str.replace("12", "#")
-            str=str.replace("13", "#")
-            str=str.replace("14", "#")
-            str=str.replace("15", "#")
-            str=str.replace("16", "#")
-            str=str.replace("17", "#")
-            str=str.replace("18", "#")
-            str=str.replace("1", "")
-            str=str.replace("2", "#")
-            str=str.replace("3", "#")
-            str=str.replace("4", "#")
-            str=str.replace("5", "#")
-            str=str.replace("6", "#")
-            str=str.replace("7", "#")
-            str=str.replace("8", "#")
-            str=str.replace("9", "#")
-            str=str.replace("##", "#")
-
-            a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-            b = str.split('#')
-
-            Outline = list(zip(a,b))
-
-    with open('result.csv', 'w', newline='') as csvfile:
-        # 建立 CSV 檔寫入器
-        writer = csv.writer(csvfile)
-        writer.writerow(["先修課程或先備能力","教師聯絡資訊","評量方式","教學方法","課程教材","參考書目","課程概述","課程學習目標","課程進度"])
-        writer.writerow([Prerequisite,Contact,Grading,Strategies,Material,References,Description,Objectives,Outline])
-    csvfile.close()
+                        Outline = list(zip(a,b))
+            with open('result.csv', 'a', newline='') as tablefile:
+                # 建立 CSV 檔寫入器
+                writer = csv.writer(tablefile)
+                writer.writerow([row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],Prerequisite,Contact,Grading,Strategies,Material,References,Description,Objectives,Outline])
+            tablefile.close()  
+            #break    
+        row_num = row_num + 1
+csvfile.close()
