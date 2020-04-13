4706a7180810fb2623cd2a46ed13b9bcbec54966
adam1214 <34540871+adam1214@users.noreply.github.com>
2020-04-13 12:09:08 +0800

    wk18 could be None.
    Solved

--- main.py
+++ main.py
@@ -35,10 +35,11 @@ with open('ncku_course.csv', 'r', newline='', encoding="utf-8") as csvfile:
     # 讀取 CSV 檔案內容
     rows = csv.reader(csvfile)
     row_num = 1
-    # 以迴圈讀取每一列
     for row in rows:
+        '''
         if row_num == 10:
             break
+        '''
         print(row_num)
         if row_num == 1:
             writer.writerow(["學院","系所名稱","系號-序號","課程碼-分班碼","屬性碼","年級","類別","科目名稱","學分","必/選修","教師姓名","已選課人數/餘額","時間","教室","課程大綱","先修課程或先備能力","教師聯絡資訊","評量方式","教學方法","課程教材","參考書目","課程概述","課程學習目標","課程進度"])
@@ -114,6 +115,25 @@ with open('ncku_course.csv', 'r', newline='', encoding="utf-8") as csvfile:
                         Material = div.text[1:]
                         
                     if div.text.find('課程進度') != -1:
+                        wk1_data = ""
+                        wk2_data = ""
+                        wk3_data = ""
+                        wk4_data = ""
+                        wk5_data = ""
+                        wk6_data = ""
+                        wk7_data = ""
+                        wk8_data = ""
+                        wk9_data = ""
+                        wk10_data = ""
+                        wk11_data = ""
+                        wk12_data = ""
+                        wk13_data = ""
+                        wk14_data = ""
+                        wk15_data = ""
+                        wk16_data = ""
+                        wk17_data = ""
+                        wk18_data = ""
+
                         wk1 = soup.find(id='wk1')
                         wk2 = soup.find(id='wk2')
                         wk3 = soup.find(id='wk3')
@@ -132,8 +152,46 @@ with open('ncku_course.csv', 'r', newline='', encoding="utf-8") as csvfile:
                         wk16 = soup.find(id='wk16')
                         wk17 = soup.find(id='wk17')
                         wk18 = soup.find(id='wk18')
+
+                        if wk1 != None:
+                            wk1_data = wk1.text
+                        if wk2 != None:
+                            wk2_data = wk2.text 
+                        if wk3 != None:
+                            wk3_data = wk3.text
+                        if wk4 != None:
+                            wk4_data = wk4.text  
+                        if wk5 != None:
+                            wk5_data = wk5.text
+                        if wk6 != None:
+                            wk6_data = wk6.text
+                        if wk7 != None:
+                            wk7_data = wk7.text
+                        if wk8 != None:
+                            wk8_data = wk8.text
+                        if wk9 != None:
+                            wk9_data = wk9.text
+                        if wk10 != None:
+                            wk10_data = wk10.text
+                        if wk11 != None:
+                            wk11_data = wk11.text
+                        if wk12 != None:
+                            wk12_data = wk12.text
+                        if wk13 != None:
+                            wk13_data = wk13.text
+                        if wk14 != None:
+                            wk14_data = wk14.text
+                        if wk15 != None:
+                            wk15_data = wk15.text
+                        if wk16 != None:
+                            wk16_data = wk16.text
+                        if wk17 != None:
+                            wk17_data = wk17.text
+                        if wk18 != None:
+                            wk18_data = wk18.text                                                 
+
                         L1 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18']
-                        L2 = [wk1.text,wk2.text,wk3.text,wk4.text,wk5.text,wk6.text,wk7.text,wk8.text,wk9.text,wk10.text,wk11.text,wk12.text,wk13.text,wk14.text,wk15.text,wk16.text,wk17.text,wk18.text]
+                        L2 = [wk1_data,wk2_data,wk3_data,wk4_data,wk5_data,wk6_data,wk7_data,wk8_data,wk9_data,wk10_data,wk11_data,wk12_data,wk13_data,wk14_data,wk15_data,wk16_data,wk17_data,wk18_data]
                         Outline = list(zip(L1,L2))
                         #print(Outline)
             writer.writerow([row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],Prerequisite,Contact,Grading,Strategies,Material,References,Description,Objectives,Outline])   
